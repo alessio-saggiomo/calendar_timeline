5ba140dcca0dd4984ca296c66e32d8bbb1be7ebc
Alessio Saggiomo <alessio.saggiomo@wisemotions.com>
2020-09-16 10:52:08 +0200

    change forecastmodel

--- lib/forecastModel.dart
+++ lib/forecastModel.dart
@@ -2,10 +2,4 @@ class ForecastModel {
   String rainChance;
   String forecastImgPath;
 
-  ForecastModel(this.rainChance, this.forecastImgPath);
-
-  @override
-  String toString() {
-    return '{ ${this.rainChance}, ${this.forecastImgPath} }';
-  }
 }
